cf9910fc150918e7fe660e4b6ad25a55d54d4913
ReflexiveFox <lorenzo.petraroli@hotmail.it>
2024-07-03 08:20:53 +0200

    Added greeting and adding numbers methods to test

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
 def main():
     return "Hello, World!"
 
+def greet(name):
+    return "Hello " + name
+
+def add_numbers(a, b):
+    return a + b
+
 if __name__ == "__main__":
-    main()
+    print(main())
+    print(greet("Lorenzo"))
+    print(add_numbers(3,4))
--- test_hello.py
+++ test_hello.py
@@ -1,9 +1,18 @@
 import unittest
-from hello import main
+from hello import main, greet, add_numbers
 
 class TestHello(unittest.TestCase):
     def test_main(self):
         self.assertEqual(main(), "Hello, World!")
+
+    def test_greet(self):
+        self.assertEqual(greet("Lorenzo"), "Hello Lorenzo!")
+        self.assertEqual(greet("Giorgia"), "Hello Giorgia!")
+
+    def test_add_numbers(self):
+        self.assertEqual(add_numbers(3,4), 7)
+        self.assertEqual(add_numbers(-1,1), 0)
+        self.assertEqual(add_numbers(-1,-10) , -11)
 
 if __name__ == "__main__":
     unittest.main()
