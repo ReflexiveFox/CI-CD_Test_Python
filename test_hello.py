e6fb93fce6aae0b8c7024508e71f60d0f3e58b62
ReflexiveFox <lorenzo.petraroli@hotmail.it>
2024-07-03 09:53:33 +0200

    Added tests for factorial

--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,5 @@
 import unittest
-from hello import main, greet, add_numbers
+from hello import main, greet, add_numbers, calculate_factorial
 
 class TestHello(unittest.TestCase):
     def test_main(self):
@@ -14,5 +14,11 @@ class TestHello(unittest.TestCase):
         self.assertEqual(add_numbers(-1,1), 0)
         self.assertEqual(add_numbers(-1,-10) , -11)
 
+    def test_calculate_factorial(self):
+        self.assertEqual(calculate_factorial(5), 120)
+        self.assertEqual(calculate_factorial(0), 1)
+        self.assertEqual(calculate_factorial(2), 2)
+
 if __name__ == "__main__":
     unittest.main()
+    
