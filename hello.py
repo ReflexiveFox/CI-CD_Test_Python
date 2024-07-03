45471370c5bf7d87609a06407dd8b7f59c5f53cb
ReflexiveFox <lorenzo.petraroli@hotmail.it>
2024-07-03 09:46:43 +0200

    Added my version for calculate factorial

--- hello.py
+++ hello.py
@@ -7,7 +7,16 @@ def greet(name):
 def add_numbers(a, b):
     return a + b
 
+def calculate_factorial(number):
+    if(number > 1):
+        return number * calculate_factorial(number-1)
+    return 1
+
+a = 3
+b = 4
+factorial_number = 0
 if __name__ == "__main__":
     print(main())
     print(greet("Lorenzo"))
-    print(add_numbers(3,4))
+    print(f"add_numbers({a}, {b}) => {add_numbers(a,b)}")
+    print(f"calculate_factorial({factorial_number}) => {calculate_factorial(factorial_number)}")
